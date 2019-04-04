60fc0c5d9a73ef3fc5ae712dd9bad54bcf1c5ebf
student <student@hubu.edu.cn>
2019-04-04 15:37:07 +0800

    4.4

---
+++ src/pages/map/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+      <i-tabs :current="current_scroll" scroll @change="handleChangeScroll">
+        <i-tab key="tab1" title="选项1"></i-tab>
+        <i-tab key="tab1" title="选项2"></i-tab>
+        <i-tab key="tab1" title="选项3"></i-tab>
+        <i-tab key="tab1" title="选项4"></i-tab>
+        <i-tab key="tab1" title="选项5"></i-tab>
+        <i-tab key="tab1" title="选项6"></i-tab>
+        <i-tab key="tab1" title="选项7"></i-tab>
+        <i-tab key="tab1" title="选项8"></i-tab>
+        <i-tab key="tab1" title="选项9"></i-tab>
+        </i-tabs>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  },
+
+  data () {
+    return {
+      current_scroll: 'tab1',
+      motto: 'Hello miniprograme',
+    }
+  },
+   components: {
+    card
+  },
+
+  methods: {
+    handleChangeScroll (event) {
+     console.log(event)
+     this.current_scroll = event.mp.detail.key
+      
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+div >>>.no-border {
+ border-width: 0pt;
+}
+div >>>.top-padding {
+  margin-top: 20pt;
+}
+</style>
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,41 +1,13 @@
 <template>
   <div>
-    <i-notice-bar icon="systemprompt" loop>
-      {{notice}}珍藏你的回忆时刻
-      </i-notice-bar>
-      <i-grid-item i-class="no-border">
-        <i-grid-item v-for="item in grids" :key="item" i-class="no-border">
-          <i-grid-label>{{item}}</i-grid-label>
-          </i-grid-item>
-           </i-grid-item>
-    <i-grid>
-      <i-grid-item i-class="no-border">
-        <i-grid-icon>
-          <image src="/static/tabs/home.png" />
-           </i-grid-icon>
-            <i-grid-label>过去</i-grid-label>
-             </i-grid-item>
-      <i-grid-item i-class="no-border">
-        <i-grid-icon>
-          <image src="/static/tabs/home.png" />
-           </i-grid-icon>
-            <i-grid-label>现在</i-grid-label>
-             </i-grid-item>
-      <i-grid-item i-class="no-border">
-        <i-grid-icon>
-          <image src="/static/tabs/home.png" />
-           </i-grid-icon>
-            <i-grid-label>未来</i-grid-label>
-             </i-grid-item>
-             </i-grid>
-             <i-panel title="精彩推荐">
-                 <i-card i-class="top-padding" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
-                 <view slot="content">内容不错</view>
-                 <view slot="footer">尾部内容</view>
-                </i-card>
-               
-            </i-panel>
-          
+    <view class="userinfo">
+      <view class="userinfo-avatar">
+    <open-data type="userAvatarUrl"></open-data>
+    </view>
+    <open-data type="userNickName"></open-data>
+    </view>
+    <i-cell-group>
+      </i-cell-group>
         </div>
     
 </template>
@@ -46,8 +18,11 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-         notice: '4.3',
-         grids: ['过去','现在','未来']
+         motto: 'Hello miniprograme',
+         userInfo: {
+           nickName: 'mpvue',
+           avatarUrl: 'http://mpvue.com/assets/logo.png'
+         }
     }
   },
 
