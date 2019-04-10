5ae34281e53cb4d66dd2c0df711637addbd9dab0
student <student@hubu.edu.cn>
2019-04-10 11:34:55 +0800

    4.10

--- functions/me/index.js
+++ functions/me/index.js
@@ -2,6 +2,22 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init()
+const db = cloud.database()
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  return await db.collection('order').add({
+    data:{
+      user: wxContext.OPENID,
+      action: wxContext.OPENID + '新增订单'
+    }
+  })
+}
+
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
