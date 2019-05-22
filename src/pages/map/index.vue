16ae171be6c9746e3bd1cadb7e3edb6504fb2bfa
student <student@hubu.edu.cn>
2019-05-22 11:16:24 +0800

    ，

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -5,7 +5,7 @@
 </i-panel>
  <i-panel title="添加相册">
                <view class="top-padding">
-                 <i-card title="风景类" extra="点击添加" thumb="/static/tabs/out.png">              
+                 <i-card  title="风景类" extra="点击添加" thumb="/static/tabs/out.png">              
                  <view slot="footer">点击添加</view>
                 </i-card>
                </view>
@@ -47,6 +47,8 @@ export default {
   },
 
   methods: {
+
+  
     handleChangeScroll (event) {
      console.log(event)
      this.current_scroll = event.mp.detail.key
